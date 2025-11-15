03939d31cedd0944a423c39476e600c72a24e959
Tianwei Liu <tianwei.liu@outlook.com>
2025-11-15 11:20:11 -0800

    Add lc #307

---
+++ 307_range_sum_query.hs
@@ -0,0 +1,11 @@
+module RangeSumQuery where
+
+type NumArray = [Int]
+
+update :: Int -> Int -> NumArray -> NumArray
+update a b xs = take a xs ++ [b] ++ drop (a + 1) xs
+
+sumRange :: Int -> Int -> NumArray -> Int
+sumRange a b xs = foldl' (+) 0 (slice a b xs)
+  where
+    slice a b = take (b - a + 1) . drop a
